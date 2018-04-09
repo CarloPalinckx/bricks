173c2dfd3472af14ebcf93b4c5346d1417335d3f
Carlo Palinckx <carlopalinckx@gmail.com>
2018-04-09 16:42:25 +0200

    Fixed Icon interpolation error

--- src/components/Icon/Icon.style.tsx
+++ src/components/Icon/Icon.style.tsx
@@ -1,17 +1,17 @@
 import { StyledComponentClass as _S } from 'styled-components';
 import styled from '../../utility/styled';
-import Icon from './Icon.template';
+import Icon, { PropsType } from './Icon.template';
 
 const StyledIcon = styled(Icon)`
     display: inline-block;
     vertical-align: middle;
-    height: ${({ size }):string => size === 'large' ? '60px' : '18px'};
-    width: ${({ size }):string => size === 'large' ? '60px' : '18px'};
+    height: ${(props:PropsType):string => props.size === 'large' ? '60px' : '18px'};
+    width: ${(props:PropsType):string => props.size === 'large' ? '60px' : '18px'};
 
     svg {
         max-height: 100%;
         max-width: 100%;
-        fill: ${({ theme, color }):string => color !== undefined ? color : 'currentColor'};
+        fill: ${(props:PropsType):string => props.color !== undefined ? props.color : 'currentColor'};
     }
 `;
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export { default as BreakpointProvider } from './components/BreakpointProvider';
 export { default as Button } from './components/Button';
+export { default as FlatButton } from './components/FlatButton';
 export { default as FoldOut } from './components/FoldOut';
 export { default as Heading } from './components/Heading';
 export { default as Icon } from './components/Icon';
+export { default as MosTheme } from './themes/MosTheme';
 export { default as Spacer } from './components/Spacer';
 export { default as SubHeading } from './components/SubHeading';
 export { default as Text } from './components/Text';
-export { default as MosTheme } from './themes/MosTheme';
