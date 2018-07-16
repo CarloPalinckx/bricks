d70d823eb439f7e547eae0d43c58a21ffdc0210f
Nico Koelewijn <nicokoelewijn@users.noreply.github.com>
2018-07-16 11:56:00 +0200

    added modal component to index (#103)

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export { default as Link } from './components/Link';
 export { default as MerchantTheme } from './themes/MerchantTheme';
 export { default as MessageStream } from './components/MessageStream';
 export { default as MosTheme } from './themes/MosTheme';
+export { default as Modal } from './components/Modal';
 export { default as Notification } from './components/Notification';
 export { default as Popover } from './components/Popover';
 export { default as PriceTag } from './components/PriceTag';
