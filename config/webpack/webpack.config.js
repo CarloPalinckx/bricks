a854e0f39f2b50a3a1b6bb6348048bbcbdb3a965
Henk Borghols <henk.borghols@gmail.com>
2018-07-19 17:10:28 +0200

    replaced close icon and updated webpack config to remove tags from svgs (#110)

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,43 +1,45 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.ts",
+    entry: './src/index.ts',
     stats: {
         assets: true,
         modules: false,
         children: false,
     },
     output: {
-        filename: "index.js",
-        path: __dirname + "/../../dist",
+        filename: 'index.js',
+        path: __dirname + '/../../dist',
         library: 'bricks',
         libraryTarget: 'umd',
         umdNamedDefine: true,
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: ['.ts', '.tsx', '.js', '.json'],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: "ts-loader",
+                loader: 'ts-loader',
                 options: {
-                    configFile: __dirname + "/../typescript/tsconfig.json",
-                }
+                    configFile: __dirname + '/../typescript/tsconfig.json',
+                },
             },
             {
                 test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.svg$/,
-                loader: 'svg-inline-loader'
+                loader: 'svg-inline-loader',
+                options: {
+                    removeTags: true,
+                    removingTags: ['title', 'desc', 'defs', 'style'],
+                },
             },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+        ],
     },
-    plugins: [
-        new UglifyJSPlugin()
-    ]
+    plugins: [new UglifyJSPlugin()],
 };
