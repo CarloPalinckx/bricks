2aa6c96c389750c094f9a06d82042094006405cf
Robin van der Voort <ro.vdv@hotmail.com>
2018-10-16 14:15:42 +0200

    removed a unnecessary line of code

--- src/components/Badge/story.tsx
+++ src/components/Badge/story.tsx
@@ -5,7 +5,6 @@ import Badge, { BadgePropsType } from '.';
 
 storiesOf('Badge', module).add('Default', () => {
     return (
-        // <Badge severity={select('severity', ['success', 'warning', 'error', 'info'], 'success') as BadgePropsType['severity']}>
         <Badge severity={select('severity', ['success', 'warning', 'error', 'info'], 'success') as BadgePropsType['severity']}>
             {text('text', '1')}
         </Badge>
