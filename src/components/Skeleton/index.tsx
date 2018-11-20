cd263ba0cb38c91d029e7464043070cf6e97fbd8
Nico Koelewijn <nico.koelewijn@live.com>
2018-11-20 11:39:52 +0100

    Rebased on master

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -1,8 +1,10 @@
 import Text from './Text';
+import Rect from './Rect';
 import _R from 'react';
 
 const Skeleton = {
     Text,
+    Rect,
 };
 
 export default Skeleton;
--- src/components/Skeleton/story.tsx
+++ src/components/Skeleton/story.tsx
@@ -3,24 +3,33 @@ import React, { SFC } from 'react';
 import Skeleton from '.';
 import { boolean, number, text } from '@storybook/addon-knobs';
 import Text from '../Text';
-
+import EmptyState from '../EmptyState';
 type DemoPropsType = {
     loading?: boolean;
     element: string;
 };
 
 const Demo: SFC<DemoPropsType> = ({ element, loading }): JSX.Element => {
-    if (loading) {
-        return <Skeleton.Text lines={number('lines', 3)} baseWidth={number('baseWidth', 80)} />;
-    }
+    if (element === 'Text') {
+        if (loading) {
+            return <Skeleton.Text lines={number('lines', 3)} baseWidth={number('baseWidth', 80)} />;
+        }
 
-    return (
-        <>
-            <Text>Leverage agile frameworks to provide a robust synopsis for high level overviews.</Text>
-            <Text>Iterative approaches to corporate strategy foster collaborative</Text>
-            <Text>thinking to further the overall value proposition.</Text>
-        </>
-    );
+        return (
+            <>
+                <Text>Leverage agile frameworks to provide a robust synopsis for high level overviews.</Text>
+                <Text>Iterative approaches to corporate strategy foster collaborative</Text>
+                <Text>thinking to further the overall value proposition.</Text>
+            </>
+        );
+    } else {
+        if (loading) {
+            return <Skeleton.Rect width={text('width', '800px')} height={text('height', '600px')} />;
+        }
+
+        return <EmptyState title="Loaded!" message="If you are reading this, you wasting your time." />;
+    }
 };
 
 storiesOf('Skeleton', module).add('Text', () => <Demo element="Text" loading={boolean('loading', true)} />);
+storiesOf('Skeleton', module).add('Rect', () => <Demo element="Rect" loading={boolean('loading', true)} />);
