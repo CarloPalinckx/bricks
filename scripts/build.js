da6b70952ee57be3274842ed6e81f4e3d6f23fe8
hborghols <henk@myonlinestore.com>
2018-12-06 10:01:32 +0100

    Deleted superfluous script

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,8 @@
-const compile = require('./compile');
+const bundles = require('./bundles');
 
 (async () => {
-  await compile();
+    console.log('Initiating build of modern and legacy bundles...\n');
+    await bundles();
+
+    console.log('Build completed!');
 })();
--- scripts/compile.js
+++
@@ -1,8 +0,0 @@
-const bundles = require('./bundles');
-
-module.exports = async () => {
-  console.log('Initiating build of modern and legacy bundles...\n');
-  await bundles();
-
-  console.log('Build completed!');
-};
